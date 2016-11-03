ad636af5b6eb372305b1338720aa83d17f6a4070
LiJunTian <1661370619@qq.com>
2016-11-03 17:08:42 +0800

    添加数据为0的情况

--- ChatDemoUI3.0/src/cn/ucai/superwechat/SuperWeChatHelper.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/SuperWeChatHelper.java
@@ -1288,7 +1288,7 @@ public class SuperWeChatHelper {
      * @return
      */
     public Map<String, User> getAppContactList() {
-        if (isLoggedIn() && appContactList == null||appContactList.size()==0) {
+        if (isLoggedIn() && (appContactList == null||appContactList.size()==0)) {
             appContactList = demoModel.getAppContactList();
         }
 
