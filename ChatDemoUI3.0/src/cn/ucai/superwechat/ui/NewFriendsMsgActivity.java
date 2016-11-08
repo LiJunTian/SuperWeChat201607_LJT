3d1ae0fee568ebf8e9f8d8ec1cbd86e37c997d99
LiJunTian <1661370619@qq.com>
2016-11-08 14:55:54 +0800

    NewFriendMsgActivity界面处理逻辑和布局

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/NewFriendsMsgActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/NewFriendsMsgActivity.java
@@ -1,10 +1,10 @@
 /**
  * Copyright (C) 2016 Hyphenate Inc. All rights reserved.
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,14 +15,20 @@ package cn.ucai.superwechat.ui;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.adapter.NewFriendsMsgAdapter;
 import cn.ucai.superwechat.db.InviteMessgeDao;
 import cn.ucai.superwechat.domain.InviteMessage;
-
-import java.util.List;
+import cn.ucai.superwechat.utils.MFGT;
 
 /**
  * Application and notification
@@ -30,22 +36,38 @@ import java.util.List;
  */
 public class NewFriendsMsgActivity extends BaseActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.em_activity_new_friends_msg);
+    @BindView(R.id.register_and_login_title_back)
+    ImageView ivBack;
+    @BindView(R.id.tv_title_text)
+    TextView tvTitleText;
 
-		ListView listView = (ListView) findViewById(R.id.list);
-		InviteMessgeDao dao = new InviteMessgeDao(this);
-		List<InviteMessage> msgs = dao.getMessagesList();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.em_activity_new_friends_msg);
+        ButterKnife.bind(this);
+        initView();
+        ListView listView = (ListView) findViewById(R.id.list);
+        InviteMessgeDao dao = new InviteMessgeDao(this);
+        List<InviteMessage> msgs = dao.getMessagesList();
 
-		NewFriendsMsgAdapter adapter = new NewFriendsMsgAdapter(this, 1, msgs);
-		listView.setAdapter(adapter);
-		dao.saveUnreadMessageCount(0);
-		
-	}
+        NewFriendsMsgAdapter adapter = new NewFriendsMsgAdapter(this, 1, msgs);
+        listView.setAdapter(adapter);
+        dao.saveUnreadMessageCount(0);
+    }
 
-	public void back(View view) {
-		finish();
-	}
+    private void initView() {
+        ivBack.setVisibility(View.VISIBLE);
+        tvTitleText.setVisibility(View.VISIBLE);
+        tvTitleText.setText("新的朋友");
+    }
+
+    public void back(View view) {
+        finish();
+    }
+
+    @OnClick(R.id.register_and_login_title_back)
+    public void onClick() {
+        MFGT.finish(this);
+    }
 }
