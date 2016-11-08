db9f5562d4ff952daacd286b8147977706bfa53c
LiJunTian <1661370619@qq.com>
2016-11-08 11:16:19 +0800

    在MainActivity中添加消息通知的显示

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/MainActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/MainActivity.java
@@ -79,10 +79,10 @@ public class MainActivity extends BaseActivity implements DMTabHost.OnCheckedCha
 //	private TextView unreadAddressLable;
 //
 //	private Button[] mTabs;
-//	private ContactListFragment contactListFragment;
+	private ContactListFragment contactListFragment;
 //	private Fragment[] fragments;
 //	private int index;
-//	private int currentTabIndex;
+	private int currentTabIndex;
 	// user logged into another device
 	public boolean isConflict = false;
 	// user account was removed
@@ -117,6 +117,7 @@ public class MainActivity extends BaseActivity implements DMTabHost.OnCheckedCha
 		ButterKnife.bind(this);
 		// runtime permission for android 6.0, just require all permissions here for simple
 		requestPermissions();
+		contactListFragment = new ContactListFragment();
 		initView();
 		ument();
 		checkAccount();
@@ -125,7 +126,6 @@ public class MainActivity extends BaseActivity implements DMTabHost.OnCheckedCha
 		inviteMessgeDao = new InviteMessgeDao(this);
 		UserDao userDao = new UserDao(this);
 //		conversationListFragment = new ConversationListFragment();
-//		contactListFragment = new ContactListFragment();
 //		SettingsFragment settingFragment = new SettingsFragment();
 //		fragments = new Fragment[] { conversationListFragment, contactListFragment, settingFragment};
 //
@@ -211,6 +211,7 @@ public class MainActivity extends BaseActivity implements DMTabHost.OnCheckedCha
 //		// select first tab
 //		mTabs[0].setSelected(true);
 
+
 		tvWeiXin.setVisibility(View.VISIBLE);
 		ivTitleAdd.setVisibility(View.VISIBLE);
 		adapter = new MainTabAdpter(getSupportFragmentManager());
@@ -218,10 +219,11 @@ public class MainActivity extends BaseActivity implements DMTabHost.OnCheckedCha
 		mLayoutViewpage.setOffscreenPageLimit(4);
 		adapter.clear();
 		adapter.addFragment(new ConversationListFragment(),getString(R.string.app_name));
-		adapter.addFragment(new ContactListFragment(),getString(R.string.contacts));
+		adapter.addFragment(contactListFragment,getString(R.string.contacts));
 		adapter.addFragment(new DiscoverFragment(),getString(R.string.discover));
 		adapter.addFragment(new ProfileFragment(),getString(R.string.me));
 		adapter.notifyDataSetChanged();
+		currentTabIndex = 0;
 		mLayoutTabhost.setChecked(0);
 
 		mLayoutTabhost.setOnCheckedChangeListener(this);
@@ -326,11 +328,12 @@ public class MainActivity extends BaseActivity implements DMTabHost.OnCheckedCha
 //                    if (conversationListFragment != null) {
 //                        conversationListFragment.refresh();
 //                    }
-//                } else if (currentTabIndex == 1) {
-//                    if(contactListFragment != null) {
-//                        contactListFragment.refresh();
-//                    }
-//                }
+//                } else
+					if (currentTabIndex == 1) {
+                    if(contactListFragment != null) {
+                        contactListFragment.refresh();
+                    }
+                }
                 String action = intent.getAction();
                 if(action.equals(Constant.ACTION_GROUP_CHANAGED)){
                     if (EaseCommonUtils.getTopActivity(MainActivity.this).equals(GroupsActivity.class.getName())) {
@@ -351,6 +354,7 @@ public class MainActivity extends BaseActivity implements DMTabHost.OnCheckedCha
 
 	@Override
 	public void onCheckedChange(int checkedPosition, boolean byUser) {
+		currentTabIndex = checkedPosition;
 		mLayoutViewpage.setCurrentItem(checkedPosition,true);
 	}
 
@@ -361,6 +365,7 @@ public class MainActivity extends BaseActivity implements DMTabHost.OnCheckedCha
 
 	@Override
 	public void onPageSelected(int position) {
+		currentTabIndex = position;
 		mLayoutTabhost.setChecked(position);
 	}
 
@@ -435,11 +440,13 @@ public class MainActivity extends BaseActivity implements DMTabHost.OnCheckedCha
 		runOnUiThread(new Runnable() {
 			public void run() {
 				int count = getUnreadAddressCountTotal();
-//				if (count > 0) {
-//					unreadAddressLable.setVisibility(View.VISIBLE);
-//				} else {
-//					unreadAddressLable.setVisibility(View.INVISIBLE);
-//				}
+				if (count > 0) {
+//					mLayoutTabhost.setVisibility(View.VISIBLE);
+					mLayoutTabhost.setHasNew(1,true);
+				} else {
+//					mLayoutTabhost.setVisibility(View.INVISIBLE);
+					mLayoutTabhost.setHasNew(1,false);
+				}
 			}
 		});
 
