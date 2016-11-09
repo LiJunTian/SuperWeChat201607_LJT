14d6544126112edb8df794233a301712d42daf40
LiJunTian <1661370619@qq.com>
2016-11-09 14:23:06 +0800

    在DBManager,Model,UserDao中添加删除好友方法

--- ChatDemoUI3.0/src/cn/ucai/superwechat/db/UserDao.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/db/UserDao.java
@@ -137,4 +137,12 @@ public class UserDao {
 	public void saveAppContactList(List<User> contactList) {
 		SuperWeChatDBManager.getInstance().saveAppContactList(contactList);
 	}
+
+	/**
+	 * delete a contact
+	 * @param username
+	 */
+	public void deleteAppContact(String username){
+		SuperWeChatDBManager.getInstance().deleteAppContact(username);
+	}
 }
