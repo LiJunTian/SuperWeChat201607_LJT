fe4bc1edaa9b3ea0577027f916fcf44551643dda
LiJunTian <1661370619@qq.com>
2016-11-10 12:52:48 +0800

    聊天室title，back键返回到首页

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/ChatFragment.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/ChatFragment.java
@@ -28,6 +28,7 @@ import com.hyphenate.chat.EMGroup;
 import com.hyphenate.chat.EMMessage;
 import com.hyphenate.chat.EMTextMessageBody;
 import cn.ucai.superwechat.Constant;
+import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.SuperWeChatHelper;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.domain.EmojiconExampleGroupData;
@@ -97,14 +98,15 @@ public class ChatFragment extends EaseChatFragment implements EaseChatFragment.E
         }
         super.setUpView();
         // set click listener
+        //聊天室title，back键返回到首页
         titleBar.setLeftLayoutClickListener(new OnClickListener() {
-
             @Override
             public void onClick(View v) {
-                if (EasyUtils.isSingleActivity(getActivity())) {
+//                if (EasyUtils.isSingleActivity(getActivity())) {
                     Intent intent = new Intent(getActivity(), MainActivity.class);
+                    intent.putExtra(I.ACTION_BACK_CONVERSION,true);
                     startActivity(intent);
-                }
+//                }
                 onBackPressed();
             }
         });
